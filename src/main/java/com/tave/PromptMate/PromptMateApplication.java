41044a5b5abb62158efc14b1942c63a94c6e2872
kim junsoo <kjs201105@naver.com>
2025-10-20 16:46:29 +0900

    Initial commit

---
+++ src/main/java/com/tave/PromptMate/PromptMateApplication.java
@@ -0,0 +1,13 @@
+package com.tave.PromptMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tave/PromptMate/PromptMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tave.PromptMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
