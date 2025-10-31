b9a7d1c4fe99bae20613ac99e70bbbba9d1038b5
subinpar <bagsubin565@gmail.com>
2025-10-31 17:40:13 +0900

    [FIX] LibraryItem 복합키 매핑 및 레포지토리 메서드 수정

--- src/main/java/com/tave/PromptMate/repository/LibraryRepository.java
+++ src/main/java/com/tave/PromptMate/repository/LibraryRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LibraryRepository extends JpaRepository<LibraryId, LibraryItem> {
+public interface LibraryRepository extends JpaRepository<LibraryItem,LibraryId> {
     boolean existsById(LibraryId id);
     Page<LibraryItem> findByUser_IdOrderByCreatedAtDesc(Long userId, Pageable pageable );
-    void deletedByUser_IdAndPrompt_Id(Long userId, Long promptId);
+    void deleteByUser_IdAndPrompt_Id(Long userId, Long promptId);
 }
