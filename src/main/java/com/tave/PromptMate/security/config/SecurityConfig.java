45e18a8734ce043b3b7c6c996aeb1b3c78083eeb
kim junsoo <kjs201105@naver.com>
2025-11-14 11:12:15 +0900

    fix: application.yml & SecurityConfig

--- src/main/java/com/tave/PromptMate/security/config/SecurityConfig.java
+++ src/main/java/com/tave/PromptMate/security/config/SecurityConfig.java
@@ -27,7 +27,8 @@ public class SecurityConfig {
     private static final String[] WHITE_LIST={
             "/login/**",
             "/swagger-ui/**",
-            "/v3/api-docs/**"
+            "/v3/api-docs/**",
+            "/api/**"
     };
 
     @Bean
