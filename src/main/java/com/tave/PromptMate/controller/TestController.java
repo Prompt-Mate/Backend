2b4957065ed77f18589368f267d69a5a9e40de7d
kim junsoo <kjs201105@naver.com>
2025-11-30 12:02:48 +0900

    test용

---
+++ src/main/java/com/tave/PromptMate/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.tave.PromptMate.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hi")
+    public String hi() {
+
+        return "Hello";
+    }
+}
