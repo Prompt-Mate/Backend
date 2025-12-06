899e6924175807b05ac1af143e6d4ca048ba07fc
subinpar <bagsubin565@gmail.com>
2025-12-06 20:18:46 +0900

    feat: 라이브러리-내가 쓴 글 인증된 사용자 기반으로 수정

--- src/main/java/com/tave/PromptMate/controller/LibraryController.java
+++ src/main/java/com/tave/PromptMate/controller/LibraryController.java
@@ -1,5 +1,6 @@
 package com.tave.PromptMate.controller;
 
+import com.tave.PromptMate.auth.dto.request.CustomUserDetails;
 import com.tave.PromptMate.dto.community.CommunityPostResponse;
 import com.tave.PromptMate.dto.library.CreateLibraryRequest;
 import com.tave.PromptMate.dto.library.LibraryResponse;
@@ -7,6 +8,7 @@ import com.tave.PromptMate.service.LibraryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -21,27 +23,42 @@ public class LibraryController {
 
     // 라이브러리에 리라이팅 결과 저장하기
     @PostMapping
-    public ResponseEntity<LibraryResponse> save(@Valid @RequestBody CreateLibraryRequest req){
-        LibraryResponse res = libraryService.save(req);
+    public ResponseEntity<LibraryResponse> save(
+            @AuthenticationPrincipal CustomUserDetails principal,
+            @Valid @RequestBody CreateLibraryRequest req){
+
+        Long userId=principal.getUserId();
+
+        LibraryResponse res = libraryService.save(req, userId);
+
         return ResponseEntity.created(URI.create("/api/libraries/" + res.id()))
                 .body(res);
     }
 
     // 내 라이브러리 목록 조회
-    @GetMapping("/my/{userId}")
-    public ResponseEntity<List<LibraryResponse>> myList(@PathVariable Long userId){
+    @GetMapping("/my")
+    public ResponseEntity<List<LibraryResponse>> myList(@AuthenticationPrincipal CustomUserDetails principal){
+
+        Long userId=principal.getUserId();
         return ResponseEntity.ok(libraryService.getMyLibraries(userId));
     }
 
     // 단건 조회
-    @GetMapping("/{id}/users/{userId}")
-    public ResponseEntity<LibraryResponse> getOne(@PathVariable Long id, @PathVariable Long userId) {
+    @GetMapping("/{id}")
+    public ResponseEntity<LibraryResponse> getOne(@PathVariable Long id,
+                                                  @AuthenticationPrincipal CustomUserDetails principal) {
+
+        Long userId= principal.getUserId();
+
         return ResponseEntity.ok(libraryService.getOne(id, userId));
     }
 
     // 삭제
-    @DeleteMapping("/{id}/users/{userId}")
-    public ResponseEntity<Void> delete(@PathVariable Long id, @PathVariable Long userId) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id,  @AuthenticationPrincipal CustomUserDetails principal) {
+
+        Long userId= principal.getUserId();
+
         libraryService.delete(id, userId);
         return ResponseEntity.noContent().build();
     }
@@ -49,10 +66,12 @@ public class LibraryController {
     //내가 작성한 게시글 조회
     @GetMapping("/my-posts")
     public ResponseEntity<List<CommunityPostResponse>>getMyPosts(
-            @RequestParam Long userId
-    ){
-        List<CommunityPostResponse> posts=libraryService.getMyPosts(userId);
-        return ResponseEntity.ok(posts);
+            @AuthenticationPrincipal CustomUserDetails principal
+            ){
+        Long userId=principal.getUserId();
+
+        List<CommunityPostResponse> responses=libraryService.getMyPosts(userId);
+
+        return ResponseEntity.ok(responses);
     }
 }
-
