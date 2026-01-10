e8d726955dced2047bdd2d1c10b6ff4b1715b8d0
park-hoyeon <hoyeonlove1004@naver.com>
2026-01-10 12:49:34 +0900

    feat: return rewriteResultId on rewrite preview

--- src/main/java/com/tave/PromptMate/dto/rewrite/RewritePreviewResponse.java
+++ src/main/java/com/tave/PromptMate/dto/rewrite/RewritePreviewResponse.java
@@ -1,6 +1,7 @@
 package com.tave.PromptMate.dto.rewrite;
 
 public record RewritePreviewResponse(
+        Long rewriteResultId,
         String rewrittenPrompt,
         Long latencyMs,
         String modelName,
